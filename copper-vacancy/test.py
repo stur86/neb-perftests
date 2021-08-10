a1a6732584cbd8c0c30c82728add9c2fa0dc7852
stur86 <simonesturniolo@gmail.com>
2021-08-10 10:52:32 +0100

    Changing CuVac files to optimise cell properly

--- copper-vacancy/test.py
+++ copper-vacancy/test.py
@@ -3,17 +3,20 @@ import numpy as np
 from ase.build import bulk
 from ase.optimize import BFGS
 from ase.neb import NEB
-from ase.io import read
+from ase.io import read, Trajectory
 from ase.geometry.geometry import get_distances
 from ase.calculators.emt import EMT
 from ase.calculators.castep import Castep
+from ase.constraints import ExpCellFilter
 
 
 def calc():
     return EMT()
 
-
 def make_geomopt(a, name, force=False):
+
+    from ase.io.castep import write_param
+
     calc = Castep(directory='cuvac-geomopt-{0}'.format(name),
                   label=name)
     if os.path.isdir(calc._directory) and not force:
@@ -25,12 +28,17 @@ def make_geomopt(a, name, force=False):
     calc.param.cut_off_energy = 500.0
     calc.param.calculate_stress = True
     calc.param.write_cell_structure = True
-    calc.cell.cell_constraints = "1 1 1\n0 0 0"
+    calc.cell.fix_all_cell = True
     calc.cell.kpoint_mp_grid = "4 4 4"
+
     a = a.copy()
     a.calc = calc
 
     calc.prepare_input_files(a, True)
+    # Overwrite cell (we need fractional coordinates)
+    a.write(calc._abs_path('{0}.cell'.format(name)), positions_frac=True)
+    
+
 
 
 N_intermediate = 3
@@ -38,7 +46,20 @@ N_cell = 2
 initial = bulk('Cu', cubic=True)
 initial *= N_cell
 
-initial.write('CuBulk.cell')
+initial.calc = calc()
+
+print("Starting bulk lattice parameter: {0} Ang".format(initial.get_cell()[0,0]))
+
+# Relax the bulk
+ecf = ExpCellFilter(initial, hydrostatic_strain=True)
+qn = BFGS(ecf)
+traj = Trajectory('CuVacBulk.traj', 'w', initial)
+qn.attach(traj)
+qn.run(fmax=0.05)
+
+print("Relaxed bulk lattice parameter: {0} Ang".format(initial.get_cell()[0,0]))
+
+initial.write('CuBulk.cell', positions_frac=True)
 
 # place vacancy near centre of cell
 D, D_len = get_distances(np.diag(initial.cell) / 2,
