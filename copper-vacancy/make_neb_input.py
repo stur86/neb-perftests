47a3e1b37a0e3ad97e1842302fe40895aca6730d
stur86 <simonesturniolo@gmail.com>
2021-08-11 15:35:11 +0100

    Created cuvac-neb input

---
+++ copper-vacancy/make_neb_input.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Create input .cell file for the NEB
+
+from ase import io
+from ase.neb import NEB
+from ase.calculators.castep import Castep
+
+a1 = io.read('cuvac-geomopt-initial/initial-out.cell')
+a2 = io.read('cuvac-geomopt-final/final-out.cell')
+
+calc = Castep()
+a1.calc = calc
+
+calc.cell.kpoint_mp_grid = [4,4,4]
+calc.cell.fix_com = False
+calc.cell.positions_abs_product = a2
+
+neb = NEB([a1, a1.copy(), a2])
+neb.interpolate()
+
+calc.cell.positions_abs_intermediate = neb.images[1]
+
+a1.write('cuvac-neb/CuVac.cell')
