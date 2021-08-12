9ce8c7cc94251b238d59ef9541cb6437da34fcd6
stur86 <simonesturniolo@gmail.com>
2021-08-12 16:25:23 +0100

    Interpolated intermediate for NEB correctly

--- copper-vacancy/make_neb_input.py
+++ copper-vacancy/make_neb_input.py
@@ -3,8 +3,8 @@
 # Create input .cell file for the NEB
 
 from ase import io
-from ase.neb import NEB
 from ase.calculators.castep import Castep
+from soprano.collection.generate import linspaceGen
 
 a1 = io.read('cuvac-geomopt-initial/initial-out.cell')
 a2 = io.read('cuvac-geomopt-final/final-out.cell')
@@ -16,12 +16,15 @@ calc.cell.kpoint_mp_grid = [4,4,4]
 calc.cell.fix_com = False
 calc.cell.positions_abs_product = a2
 
-neb = NEB([a1, a1.copy(), a2])
-neb.interpolate()
+lgen = linspaceGen(a1, a2, 3, True)
+images = [im for im in lgen]
 
-calc.cell.positions_abs_intermediate = neb.images[1]
+# neb = NEB([a1, a1.copy(), a2])
+# neb.interpolate()
 
-for i, im in enumerate(neb.images):
+calc.cell.positions_abs_intermediate = images[1]
+
+for i, im in enumerate(images):
     io.write('CuVacNebIm{0}.cell'.format(i), im)
 
 a1.write('cuvac-neb/CuVac.cell')
