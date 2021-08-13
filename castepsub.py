52d202cab7cec4fc075faf6fe8c7cb332bd3cbae
Simone Sturniolo <scarf238@ui3.scarf.rl.ac.uk>
2021-08-13 15:41:18 +0100

    Added defaults file to castepsub.py

--- castepsub.py
+++ castepsub.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
+import json
 import argparse as ap
 import subprocess as sp
+
+defaults = json.load(open('csub_default.json'))
 
 parser = ap.ArgumentParser()
 parser.add_argument('seedname', type=str, default=None,
                     help="""CASTEP seedname.""")
-parser.add_argument('-n', type=int, default=16,
+parser.add_argument('-n', type=int, default=defaults['n'],
                     help="""Number of cores""")
-parser.add_argument('-t', type=str, default='02:00:00',
+parser.add_argument('-t', type=str, default=defaults['t'],
                     help="""Runtime""")
-parser.add_argument('-cp', type=str, default='castep.mpi',
+parser.add_argument('-cp', type=str, default=defaults['cp'],
                     help="""CASTEP path""")
 args = parser.parse_args()
 
