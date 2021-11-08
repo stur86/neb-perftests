f3025c1bcca9ff7d28b2e44e1c85f0851842921f
stur86 <simonesturniolo@gmail.com>
2021-11-08 10:49:58 +0000

    Added Morse potential test

---
+++ copper-vacancy-morse/make_neb_input.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# Create input .cell file for the NEB
+
+import numpy as np
+from ase import io
+from ase.build import bulk, make_supercell
+from ase.calculators.castep import Castep
+from ase.calculators.morse import MorsePotential
+from ase.optimize import BFGS
+from soprano.collection.generate import linspaceGen
+from soprano.properties.linkage import LinkageList
+
+A = 4.0
+DE = 1.0   # eV
+nnr = 2.55 # Angstrom, nearest neighbour distance
+
+cu = bulk('Cu', a=nnr*2**0.5, cubic=True)
+
+cu3x3 = make_supercell(cu, np.eye(3)*3)
+
+# Remove the most central atom to create a vacancy
+C = np.ones(3)*cu3x3.get_cell()[0,0]*0.5
+pos = cu3x3.positions
+i1vac = np.argmin(np.linalg.norm(pos-C, axis=1))
+p1vac = pos[i1vac]
+
+cuvac = cu3x3.copy()
+del(cuvac[i1vac])
+
+# Now the one closest to it
+i2vac = np.argmin(np.linalg.norm(cuvac.positions-p1vac, axis=1))
+
+cuvac_reac = cuvac.copy()
+cuvac_prod = cuvac_reac.copy()
+cuvac_prod[i2vac].position = p1vac
+
+# Now optimise with Morse Potential
+def makeMorseCalc():
+    return MorsePotential(epsilon=DE, rho0=A, r0=nnr)
+
+cuvac_reac.calc = makeMorseCalc()
+cuvac_prod.calc = makeMorseCalc()
+
+dyn = BFGS(cuvac_reac)
+dyn.run(fmax=0.05)
+
+dyn = BFGS(cuvac_prod)
+dyn.run(fmax=0.05)
+
+# Interpolate
+lgen = linspaceGen(cuvac_reac, cuvac_prod, 3, True)
+images = [im for im in lgen]
+cuvac_intm = images[1]
+
+# Now go for CASTEP
+cuvac_reac.calc = Castep(directory='cuvac-neb-morse', label='CuVacMorse')
+cuvac_reac.calc.cell.positions_abs_product = cuvac_prod
+cuvac_reac.calc.cell.positions_abs_intermediate = cuvac_intm
+cuvac_reac.calc.cell.fix_com = False
+
+# Morse potential
+
+# Convert to AUs
+nnr_bohr = nnr*1.88973
+DE_Ha = DE*0.0367493
+# Derived constant
+k = 2*A**2/nnr_bohr**2*DE_Ha
+
+cuvac_reac.calc.param.task = 'transitionstatesearch'
+cuvac_reac.calc.param.tssearch_method = 'neb'
+cuvac_reac.calc.param.tssearch_neb_max_iter = 100
+cuvac_reac.calc.param.tssearch_max_path_points = 11
+cuvac_reac.calc.param.tssearch_neb_climbing = True
+cuvac_reac.calc.param.devel_code = """PP=T
+pp: MORS=T MORS_R={R} MORS_K={K} MORS_D={D} :endpp
+""".format(R=nnr_bohr, K=k, D=DE_Ha)
+
+cuvac_reac.calc.prepare_input_files(cuvac_reac)
